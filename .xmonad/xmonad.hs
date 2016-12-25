2615e13637c8cd95d4c55d1d46be5b92dc12b60c
amilkov3 <amilkov3@gmail.com>
2016-12-24 21:07:59 -0500

    xmonad

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,26 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.SetWMName
+import System.IO
+
+main :: IO()
+main = do
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/amilkov3/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        }
+        , modMask = mod4Mask     -- Rebind Mod to the Windows key
+        , handleEventHook = handleEventHook defaultConfig <+> docksEventHook 
+        , startupHook = setWMName "LG3D"
+        } `additionalKeys`
+        [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock; xset dpms force off")
+        , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+        , ((0, xK_Print), spawn "scrot")
+        ]
+
